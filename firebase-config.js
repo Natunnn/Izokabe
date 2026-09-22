0618e4d09bd0e0c5878808a8326430f47d3dba5c
Natunnn <nathaniel130204@gmail.com>
2026-09-22 16:08:17 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+// Konfigurasi Firebase untuk proyek Izokabe
+const firebaseConfig = {
+  apiKey: "AIza...", // Ganti dengan apiKey Anda
+  authDomain: "izokabe-work.firebaseapp.com",
+  projectId: "izokabe-work",
+  storageBucket: "izokabe-work.appspot.com",
+  messagingSenderId: "1234567890",
+  appId: "1:1234567890:web:abcdef123456"
+};
+
+export default firebaseConfig;
